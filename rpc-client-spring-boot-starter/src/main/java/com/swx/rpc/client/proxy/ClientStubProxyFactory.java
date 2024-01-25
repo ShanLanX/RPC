9150997ca9d41aa5e3934ed52d95174778743456
孙伟祥 <1083596975@qq.com>
2024-01-25 16:36:38 +0800

    完成了rpc的基本功能

--- rpc-client-spring-boot-starter/src/main/java/com/swx/rpc/client/proxy/ClientStubProxyFactory.java
+++ rpc-client-spring-boot-starter/src/main/java/com/swx/rpc/client/proxy/ClientStubProxyFactory.java
@@ -13,8 +13,11 @@ public class ClientStubProxyFactory {
 
     // T为被代理的对象 执行T中的任何方法都会由句柄中的invoker增强
 
-    public <T> T getProxy(Class<T> clazz, String version, DiscoveryService discoveryService, RpcClientProperties rpcClientProperties){
+    //This method returns a proxy instance of the given class, with the given version, using the given discovery service and RPC client properties
+public <T> T getProxy(Class<T> clazz, String version, DiscoveryService discoveryService, RpcClientProperties rpcClientProperties){
+        //Compute the proxy instance if it is not already present in the object cache
         return (T)objectCache.computeIfAbsent(clazz,clz->
+             //Create a new proxy instance using the given class, classloader, class array and ClientStubInvocationHandler
              Proxy.newProxyInstance(clz.getClassLoader(),new Class[]{clz},new ClientStubInvocationHandler(discoveryService,rpcClientProperties,clz,version))
         );
     }
